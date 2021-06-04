54bbb68b11d0128bae5a4e1a0753157d68f29095
Joe McCleary <joemccleary@hotmail.co.uk>
2021-06-04 15:33:42 +0100

    postlist added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
 import jsonPlaceholder from '../api/jsonPlaceholder';
 
-export const fetchPosts = () => async dispatch => {
-    const response = jsonPlaceholder.get('/posts')
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts');
 
-    dispatch ({type: 'FETCH_POSTS', payload: response.data});
+    dispatch({ type: 'FETCH_POSTS', payload: response.data})
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`./users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data})
+}
